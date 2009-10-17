3e505b49f67456cc8cc5554cac758ffda11e295d
Alvaro Rodriguez <alvaro@viaro.net>
2009-10-17 19:18:29 +0000

    Spanish messages for the new plugin in xinha 0.96

--- learning-content-patch/InsertGlossaryEntry/lang/es.js
+++ learning-content-patch/InsertGlossaryEntry/lang/es.js
@@ -1,1 +1,17 @@
-{};
+// I18N constants
+// LANG: "es", ENCODING: UTF-8
+{
+  "Glossary": "Glosario",
+  "Word": "Término",
+  "Definition": "Definición",
+  "OK": "Aceptar",
+  "Cancel": "Cancelar",
+  "Remove link": "Quitar enlace",
+  "Word for the glossary": "Término para el glosario",
+  "Definition of the word for the glossary": "Definición del término para el glosario",
+  "You need to select some text before creating a link": "Necesita seleccionar un texto antes de crear el enlace",
+  "Your text should not have link tags": "El texto seleccionado no debe contener enlaces",
+  "You must select only a text in the same line to add the glossary entry": "Debe seleccionar un texto dentro de la misma línea para poder agregarle una entrada en el glosario",
+  "You cannot add a link to another link": "No puede agregar un enlace a otro enlace",
+  "That word already exists": "Esa palabra ya existe"
+};
