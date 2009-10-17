138f574fa5fe15fcfe2fcc1204d41639caa0692b
Alvaro Rodriguez <alvaro@viaro.net>
2009-10-17 19:13:26 +0000

    Do not patch the theme-zen package since the alert messages are displayed differently

--- learning-content-patch/script.sh
+++ learning-content-patch/script.sh
@@ -5,5 +5,4 @@ patch -p0 -i learning-content/learning-content-patch/evaluation-diff.patch
 patch -p0 -i learning-content/learning-content-patch/assessment-diff.patch
 patch -p0 -i learning-content/learning-content-patch/forums-diff.patch
 patch -p0 -i learning-content/learning-content-patch/acs-subsite-diff.patch
-patch -p0 -i learning-content/learning-content-patch/theme-zen-diff.patch
 cp -r learning-content/learning-content-patch/InsertGlossaryEntry/ acs-templating/www/resources/xinha-nightly/plugins/
